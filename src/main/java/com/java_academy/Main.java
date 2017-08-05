f8971a0749550934340e87e2ec31f3ec6e1dbe78
Bartlomiej Janik <bartlomiej9519@gmail.com>
2017-08-05 14:03:11 +0200

    Refactoring main

--- src/main/java/com/java_academy/Main.java
+++ src/main/java/com/java_academy/Main.java
@@ -5,12 +5,10 @@ import com.java_academy.gui.Controller;
 import com.java_academy.logic.model.MessageObject;
 import com.java_academy.network.Connector;
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 
 public class Main extends Application {
 
